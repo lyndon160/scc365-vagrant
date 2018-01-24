a8cc21d6cc1e63e3f87d6380c252705c391621bb
Lyndon Fawcett <l.fawcett1@lancaster.ac.uk>
2018-01-24 14:57:42 +0000

    Updated with helper classes

--- Practical.java
+++ Practical.java
@@ -16,7 +16,13 @@ import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+
 import net.floodlightcontroller.packet.Ethernet;
+import net.floodlightcontroller.packet.IPv4;
+import net.floodlightcontroller.packet.IPv6;
+import net.floodlightcontroller.packet.TCP;
+import net.floodlightcontroller.packet.UDP;
+import net.floodlightcontroller.packet.ICMP;
 
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.match.Match;
@@ -29,10 +35,24 @@ import org.projectfloodlight.openflow.protocol.action.OFAction;
 import org.projectfloodlight.openflow.protocol.action.OFActionOutput;
 import org.projectfloodlight.openflow.protocol.OFVersion;
 
+import org.projectfloodlight.openflow.types.DatapathId;
+import org.projectfloodlight.openflow.types.EthType;
+import org.projectfloodlight.openflow.types.IPv4Address;
+import org.projectfloodlight.openflow.types.IPv6Address;
+import org.projectfloodlight.openflow.types.IpProtocol;
+import org.projectfloodlight.openflow.types.ICMPv4Type;
+import org.projectfloodlight.openflow.types.ICMPv4Code;
+import org.projectfloodlight.openflow.types.MacAddress;
+import org.projectfloodlight.openflow.types.OFBufferId;
+import org.projectfloodlight.openflow.types.OFGroup;
+import org.projectfloodlight.openflow.types.OFPort;
+import org.projectfloodlight.openflow.types.OFVlanVidMatch;
+import org.projectfloodlight.openflow.types.TableId;
+import org.projectfloodlight.openflow.types.U64;
+import org.projectfloodlight.openflow.types.VlanVid;
 import org.projectfloodlight.openflow.types.OFPort;
 import org.projectfloodlight.openflow.types.U16;
-import org.projectfloodlight.openflow.types.OFBufferId;
-import org.projectfloodlight.openflow.types.MacAddress;
+
 import org.projectfloodlight.openflow.util.HexString;
 
 import org.slf4j.Logger;
@@ -41,8 +61,8 @@ import org.slf4j.LoggerFactory;
 public class Practical implements IFloodlightModule, IOFMessageListener {
 
 	protected static Logger log = LoggerFactory.getLogger(Practical.class);
-    	protected IFloodlightProviderService floodlightProvider;
-    	protected HashMap<Long,Integer> macToPort;
+	protected IFloodlightProviderService floodlightProvider;
+	protected HashMap<MacAddress,Integer> macToPort;
 
 	@Override
 	public String getName() {
@@ -63,9 +83,9 @@ public class Practical implements IFloodlightModule, IOFMessageListener {
 
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
-    		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
-    		l.add(IFloodlightProviderService.class);
-    		return l;
+		Collection<Class<? extends IFloodlightService>> l = new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFloodlightProviderService.class);
+		return l;
 	}
 
 	@Override
@@ -76,98 +96,173 @@ public class Practical implements IFloodlightModule, IOFMessageListener {
 
 	@Override
 	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
-    		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
-    		log = LoggerFactory.getLogger(Practical.class);
-    		macToPort = new HashMap<Long,Integer>();
+		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		log = LoggerFactory.getLogger(Practical.class);
+		macToPort = new HashMap<MacAddress,Integer>();
 	}
 
 	@Override
 	public void startUp(FloodlightModuleContext context) {
-    		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
 	}
 
 	@Override
 	/* Handle a packet message - called every time a packet is received*/
-   	public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
-
-        	//Get Packet-In message
-        	OFPacketIn pi = (OFPacketIn) msg;
-
-        	Match match = pi.getMatch();
-
-		MacAddress sourceMac = match.get(MatchField.ETH_SRC);
-		//String readableSourceMac = sourceMac.toString();
-		//log.debug("Packet with source MAC Address: {} came in port : {}", readableSourceMac, pi.getInPort());
+	public Command receive(IOFSwitch sw, OFMessage msg, FloodlightContext cntx) {
+		
+		//Get Packet-In message
+		OFPacketIn pi = (OFPacketIn) msg;
 
 		//Flood Packet to all ports
-		writePacketToPort(sw, pi, OFPort.FLOOD.getShortPortNumber(), cntx);
+		writePacketToPort(sw, pi, OFPort.FLOOD.getPortNumber());
 
 		//Print debug message
 		log.debug("Flooding packet to all ports");
 
 		//Allow Floodlight to continue processing the packet
-        	return Command.CONTINUE;
-    	}
+		return Command.CONTINUE;
+	}
 
-    /* Write a packet out to a specific port */
-    public void writePacketToPort (IOFSwitch sw, OFPacketIn pi, int outPort,  FloodlightContext cntx) {
-        //build packout
-	OFPacketOut.Builder pob = sw.getOFFactory().buildPacketOut();
-	pob.setBufferId(pi.getBufferId());
-	pob.setInPort((pi.getVersion().compareTo(OFVersion.OF_12) < 0 ? pi.getInPort() : pi.getMatch().get(MatchField.IN_PORT)));
-       	pob.setXid(pi.getXid());
+	/* Write a packet out to a specific port */
+	public void writePacketToPort (IOFSwitch sw, OFPacketIn pi, int outPort) {
+		//build packout
+		OFPacketOut.Builder pob = sw.getOFFactory().buildPacketOut();
+		pob.setBufferId(pi.getBufferId());
+		pob.setInPort((pi.getVersion().compareTo(OFVersion.OF_12) < 0 ? pi.getInPort() : pi.getMatch().get(MatchField.IN_PORT)));
+		pob.setXid(pi.getXid());
 
-	//set actions
-	OFActionOutput.Builder actionBuilder = sw.getOFFactory().actions().buildOutput();
-        actionBuilder.setPort(OFPort.ofInt(outPort));
-	pob.setActions(Collections.singletonList((OFAction) actionBuilder.build()));
+		//set actions
+		OFActionOutput.Builder actionBuilder = sw.getOFFactory().actions().buildOutput();
+		actionBuilder.setPort(OFPort.ofInt(outPort));
+		pob.setActions(Collections.singletonList((OFAction) actionBuilder.build()));
 
-	// set data if it is included in the packetin
-        if (pi.getBufferId() == OFBufferId.NO_BUFFER) {
-            byte[] packetData = pi.getData();
-            pob.setData(packetData);
-        }
+		// set data if it is included in the packetin
+		if (pi.getBufferId() == OFBufferId.NO_BUFFER) {
+			byte[] packetData = pi.getData();
+			pob.setData(packetData);
+		}
 
-	sw.write(pob.build());
-    }
+		sw.write(pob.build());
+	}
 
-    /* Install a flow-mod with given parameters */
-    private void installFlowMod(IOFSwitch sw, OFPacketIn pi, Match match, int outPort, int idleTimeout, int hardTimeout, FloodlightContext cntx) {
-	OFFlowAdd.Builder fmb = sw.getOFFactory().buildFlowAdd();
-	fmb.setBufferId(OFBufferId.of(-1));
-        fmb.setOutPort(OFPort.ofInt(outPort));
-        fmb.setMatch(match);
-        fmb.setIdleTimeout(idleTimeout);
-        fmb.setHardTimeout(hardTimeout);
-        fmb.setPriority(100);
+	/* Install a flow-mod with given parameters */
+	private void installFlowMod(IOFSwitch sw, OFPacketIn pi, Match match, int outPort, int idleTimeout, int hardTimeout) {
+		OFFlowAdd.Builder fmb = sw.getOFFactory().buildFlowAdd();
+		fmb.setBufferId(OFBufferId.of(-1));
+		fmb.setOutPort(OFPort.ofInt(outPort));
+		fmb.setMatch(match);
+		fmb.setIdleTimeout(idleTimeout);
+		fmb.setHardTimeout(hardTimeout);
+		fmb.setPriority(100);
 
-	//add action
-	OFActionOutput.Builder actionBuilder = sw.getOFFactory().actions().buildOutput();
-	actionBuilder.setPort(OFPort.ofInt(outPort));
-	fmb.setActions(Collections.singletonList((OFAction) actionBuilder.build()));
+		//add action
+		OFActionOutput.Builder actionBuilder = sw.getOFFactory().actions().buildOutput();
+		actionBuilder.setPort(OFPort.ofInt(outPort));
+		fmb.setActions(Collections.singletonList((OFAction) actionBuilder.build()));
 
-        sw.write(fmb.build());
-    }
+		sw.write(fmb.build());
+	}
 
-    private OFMessage createHubFlowMod(IOFSwitch sw, OFMessage msg) {
-        OFPacketIn pi = (OFPacketIn) msg;
-        OFFlowAdd.Builder fmb = sw.getOFFactory().buildFlowAdd();
-        fmb.setBufferId(pi.getBufferId())
-        .setXid(pi.getXid());
 
-        // set actions
-        OFActionOutput.Builder actionBuilder = sw.getOFFactory().actions().buildOutput();
-        actionBuilder.setPort(OFPort.FLOOD);
-        fmb.setActions(Collections.singletonList((OFAction) actionBuilder.build()));
+	/**
+	* Create a match containing Ethernet addresses and type; Vlan IDs; IPv4/IPv6 addresses and proto; TCP/UDP ports; ICMP type and codes.
+	*
+	* @param sw, the switch on which the packet was received
+	* @param inPort, the ingress switch port on which the packet was received
+	* @return a composed Match object based on the provided information
+	*/
+	protected Match createMatchFromPacket(IOFSwitch sw, OFPacketIn pi) {
+		Ethernet eth = new Ethernet();
+		eth.deserialize(pi.getData(), 0, pi.getTotalLen());
 
-        return fmb.build();
-    }
+		VlanVid vlan = VlanVid.ofVlan(eth.getVlanID());
+		MacAddress srcMac = eth.getSourceMACAddress();
+		MacAddress dstMac = eth.getDestinationMACAddress();
+
+		Match.Builder mb = sw.getOFFactory().buildMatch();
+		mb.setExact(MatchField.IN_PORT, (pi.getVersion().compareTo(OFVersion.OF_12) < 0 ? pi.getInPort() : pi.getMatch().get(MatchField.IN_PORT)));
+
+		//mac
+		mb.setExact(MatchField.ETH_SRC, srcMac).setExact(MatchField.ETH_DST, dstMac);
+
+		//vlan
+		if (!vlan.equals(VlanVid.ZERO)) {
+			mb.setExact(MatchField.VLAN_VID, OFVlanVidMatch.ofVlanVid(vlan));
+		}
+
+		if (eth.getEtherType() == EthType.IPv4) { /* shallow check for equality is okay for EthType */
+			IPv4 ip = (IPv4) eth.getPayload();
+			IPv4Address srcIp = ip.getSourceAddress();
+			IPv4Address dstIp = ip.getDestinationAddress();
+
+			//ipv4
+			mb.setExact(MatchField.ETH_TYPE, EthType.IPv4)
+			.setExact(MatchField.IPV4_SRC, srcIp)
+			.setExact(MatchField.IPV4_DST, dstIp);
+
+			/*
+			* Take care of the ethertype if not included earlier,
+			* since it's a prerequisite for transport ports.
+			*/
+			mb.setExact(MatchField.ETH_TYPE, EthType.IPv4);
+
+			//transport
+			if (ip.getProtocol().equals(IpProtocol.TCP)) {
+				TCP tcp = (TCP) ip.getPayload();
+				mb.setExact(MatchField.IP_PROTO, IpProtocol.TCP)
+				.setExact(MatchField.TCP_SRC, tcp.getSourcePort())
+				.setExact(MatchField.TCP_DST, tcp.getDestinationPort());
+			} else if (ip.getProtocol().equals(IpProtocol.UDP)) {
+				UDP udp = (UDP) ip.getPayload();
+				mb.setExact(MatchField.IP_PROTO, IpProtocol.UDP)
+				.setExact(MatchField.UDP_SRC, udp.getSourcePort())
+				.setExact(MatchField.UDP_DST, udp.getDestinationPort());
+			} else if (ip.getProtocol().equals(IpProtocol.ICMP)){
+				ICMP icmp = (ICMP) ip.getPayload();
+				mb.setExact(MatchField.IP_PROTO, IpProtocol.ICMP)
+				.setExact(MatchField.ICMPV4_TYPE, ICMPv4Type.of(icmp.getIcmpType()))
+				.setExact(MatchField.ICMPV4_CODE, ICMPv4Code.of(icmp.getIcmpCode()));
+			}
+		} else if (eth.getEtherType() == EthType.ARP) { /* shallow check for equality is okay for EthType */
+			mb.setExact(MatchField.ETH_TYPE, EthType.ARP);
+		} else if (eth.getEtherType() == EthType.IPv6) {
+			IPv6 ip = (IPv6) eth.getPayload();
+			IPv6Address srcIp = ip.getSourceAddress();
+			IPv6Address dstIp = ip.getDestinationAddress();
+
+			//ipv6
+			mb.setExact(MatchField.ETH_TYPE, EthType.IPv6)
+			.setExact(MatchField.IPV6_SRC, srcIp)
+			.setExact(MatchField.IPV6_DST, dstIp);
+
+
+			/*
+			* Take care of the ethertype if not included earlier,
+			* since it's a prerequisite for transport ports.
+			*/
+			mb.setExact(MatchField.ETH_TYPE, EthType.IPv6);
+
+			//transport
+			if (ip.getNextHeader().equals(IpProtocol.TCP)) {
+				TCP tcp = (TCP) ip.getPayload();
+				mb.setExact(MatchField.IP_PROTO, IpProtocol.TCP)
+				.setExact(MatchField.TCP_SRC, tcp.getSourcePort())
+				.setExact(MatchField.TCP_DST, tcp.getDestinationPort());
+			} else if (ip.getNextHeader().equals(IpProtocol.UDP)) {
+				UDP udp = (UDP) ip.getPayload();
+				mb.setExact(MatchField.IP_PROTO, IpProtocol.UDP)
+				.setExact(MatchField.UDP_SRC, udp.getSourcePort())
+				.setExact(MatchField.UDP_DST, udp.getDestinationPort());
+			}
+		}
+		return mb.build();
+	}
 
 
 
 	@Override
-    public Collection<Class<? extends IFloodlightService>> getModuleServices() {
-        // We don't provide any services, return null
-        return null;
-    }
+	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
+		// We don't provide any services, return null
+		return null;
+	}
 }
